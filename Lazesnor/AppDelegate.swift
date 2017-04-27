9e2e26eafb9694caf471be623aef5ac51b96e8e1
Tong Lin <tonglin.t91@gmail.com>
2017-04-27 13:29:48 -0400

    add google maps SDK

--- Lazesnor/AppDelegate.swift
+++ Lazesnor/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyDup4eFXeqQSIa_Lk4cBI_nbGBrnL0_GKM")
+        //GMSPlacesClient.provideAPIKey("YOUR_API_KEY")
+        
+        
         return true
     }
 
